8ece735b8385dae4e757f4d10cc3c11c46bcaea5
CoderGirlKenya <kenya78hami@gmail.com>
2021-06-18 18:44:25 -0500

    updated js

--- script.js
+++ script.js
@@ -2,70 +2,66 @@
 let missionTarget = document.getElementById("missionTarget");
 let launchForm = document.getElementById("launchForm");
 let launchStatusCheck = document.getElementById("launchStatusCheck");
-let pilotName = document.getElementById("pilotName");
-let copilotName = document.getElementById("copilotName");
-let fuelLevel = document.getElementById("fuelLevel");
-let cargoMass = document.getElementById("cargoMass");
+// let pilotName = document.querySelector("input[name=pilotName]");
+//     let copilotName = document.querySelector("input[name=copilotName]");
+//     let fuelLevel = document.querySelector("input[name=fuelLevel]");
+//     let cargoMass = document.querySelector("input[name=cargoMass]");
 let button = document.getElementById("formSubmit");
 
 window.addEventListener("load", function() {
- // let form = document.getElementById("form");
+ let form = document.querySelector("form");
   let button = document.getElementById("formSubmit");
    button.addEventListener("click", function(event){
-    let pilotName = document.querySelector("input[name=pilotName]");
-    let copilotName = document.querySelector("input[name=copilotName]");
-    let fuelLevel = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
+     event.preventDefault();
+    let pilotName = document.getElementById("pilotName");
     console.log(pilotName.value);
-      if(pilotName.value === ""){
-        
-        event.preventDefault();
-         }
-       if (copilotName.value === "" ){
-        console.log(copilotName);
-         event.preventDefault();
-       }
-        if(fuelLevel.value === Number){
-         console.log(fuelLevel);
-         event.preventDefault();
-       }
-       if(cargoMass.value === Number){
-         console.log(cargoMass);
-         event.preventDefault();
-       } else{
-            alert("All fields are required!");
-                  event.preventDefault();
-      };
+    let copilotName = document.querySelector("input[name=copilotName]");
+    console.log(copilotName.value);
+    form.addEventListener("submit", function(event){
+      event.preventDefault();
+    if (isNaN(pilotName.value) || isNaN(copilotName.value)){
+      console.log(pilotName.value);
+    } else if (fuelLevel.value !== Number || cargoMass.value !== Number){
+      alert("Not a number");
+    } else {
+      alert("All fields are required!");
+    };            
+          
+    
+    
+    
+    
+      
+     
+  
+
+      // if(pilotName.value === ""|| copilotName.value === ""){
+      //   console.log(copilotName);
+      // }
+      //   if(fuelLevel.value === Number || cargoMass.value === Number){
+      //    console.log(fuelLevel);
+      
+      //  }
+        // else {
+        //     alert("All fields are required!");
+        //           event.preventDefault();
+      
 
       
       
       let faultyItems = document.getElementById("faultyItems")
+      
+      
+      
       if(fuelLevel.value < 10000){
-        `faultyItems {
-            visibility: visible;
-            color: red;
-         }
-         <li id=${fuelStatus.value}>There is not enough fuel for the journey</li>
-         <h2 id="launchStatus">Shuttle not ready for launch</h2>
-        } 
-         `
-          
-        div.innerHTML += `
-         
-        <div id="launchStatusCheck">
-            <h2 id="launchStatus">Awaiting Information Before Launch</h2>
-            <div  id="faultyItems">
-                <ol>
-                    <li id=${pilotStatus.value}>Pilot Ready</li>
-                    <li id=${copilotStatus.value}>Co-pilot Ready</li>
-                    <li id=${fuelStatus.value}>Fuel level high enough for launch</li>
-                    <li id=${cargoStatus.value}>Cargo mass low enough for launch</li>
-                </ol>
-            </div>
-        </div>
-        `;    
+            faultyItems.style.color = "red";
+            faultyItems.style.visibility = "visible";
+         fuelStatus.innerHTML = "There is not enough fuel for the journey";
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+        };    
 
-      };
+      
+    });
     });
   });
    
