b77aac06334815c251421166b58b5ffe8cbee313
CoderGirlKenya <kenya78hami@gmail.com>
2021-06-22 21:43:10 -0500

    updated co pilot

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ window.addEventListener("load", function() {
   
     }
      else if (isNaN(pilotName.value)=== false ){
-      // copilotStatus.innerHTML = "Co-Pilot is not ready";
-      alert("Pilot should be a name(string)");
+          alert("Pilot should be a name(string)");
               console.log(isNaN(pilotName.value));
+              
       } 
       else if (isNaN(copilotName.value) === false){
         alert("Co-pilot should be a name(string)");
@@ -53,12 +53,13 @@ window.addEventListener("load", function() {
         } 
         else if (isNaN(cargoMass.value) === true){
           alert("Cargo Mass should be a number");
+          //launchStatus.innerHTML = "Shuttle not ready for launch";
         }
 
     changes();
      
     function changes(){
-    if ((pilotName.value === "" || copilotName.value === ""||fuelLevel.value=== ""|| cargoMass.value === "")){
+    if (isNaN(pilotName.value) === false || copilotName.value === ""|| isNaN(fuelLevel.value) === true|| isNaN(cargoMass.value) === true){
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
        } else {
@@ -66,46 +67,44 @@ window.addEventListener("load", function() {
          launchStatus.style.color = " green";
        }
 
-      if (isNaN(pilotName.value) === true){
-        pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`;
-      faultyItems.style.visibility= "visible";
-      } 
-      else {
+        if (isNaN(pilotName.value) === false){
         pilotStatus.innerHTML = "Pilot is not ready";
+      //faultyItems.style.visibility= "visible";
+     // launchStatus.innerHTML = "Shuttle not ready for launch";
+      launchStatus.style.color = "red";;
+        } else {
+        pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`;
       faultyItems.style.visibility= "visible";
       //launchStatusCheck.innerHTML = "Shuttle not ready for launch";
       }
       
-         if (isNaN(copilotName.value) === false ){
+          if (isNaN(copilotName.value) === false ){
               copilotStatus.innerHTML = "Co-Pilot is not ready";
              launchStatus.innerHTML = "Shuttle not ready for launch";
+             launchStatus.style.color = "red";
             } else {
               copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready`;
             }
-          if (fuelLevel.value < 10000){
+            if (fuelLevel.value < 10000){
             faultyItems.style.visibility = "visible";
          fuelStatus.innerHTML = "There is not enough fuel for the journey";
          launchStatus.innerHTML = "Shuttle not ready for launch";
          launchStatus.style.color = "red";
-          }    
+          }else{
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+          }
       
-            if(cargoMass.value >= 10000){
+            if(cargoMass.value >= 10000 ){
           faultyItems.style.visibility = "visible";
           cargoStatus.innerHTML = "There is too much mass for the shuttle to take off";
           launchStatus.innerHTML = "Shuttle not ready for launch";
            launchStatus.style.color = "red";
             //faultyItems.style.color = "red";
+           } else{
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
            }
            
-             else if (fuelLevel.value >= 10000 && cargoMass.value < 10000) {
-          
-          //launchStatus.innerHTML = "Shuttle is ready for launch";
-          cargoStatus.innerHTML = "Cargo mass low enough for launch";
-          fuelStatus.innerHTML = "Fuel level high enough for launch";
-          faultyItems.style.visibility = "visible";
-          //pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`;
-          //copilotStatus.innerHTML = `Co-Pilot ${copilotName.value} is ready`;
-            }
+             
     };      
   });      
     
